aabc354fd4c4c6fea2dbaf7e8969be66158c0062
emanuelleao <eemanuelleao@gmail.com>
2020-08-13 11:21:24 -0300

    aplyed middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -14,44 +14,69 @@
 
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('login', 'AuthController@login');
-    $router->post('refreshToken', 'AuthController@refreshToken');
-    $router->post('checkToken', 'AuthController@checkToken');
-    $router->post('logout', 'AuthController@logout');
 
-    $router->group(['prefix' => 'user'], function () use ($router) {
-        $router->get('/getAll', 'UserController@getAllUsers');
-        $router->get('/{id}/get', 'UserController@getUser');
-        $router->get('/{id}/systems', 'UserController@getSystems');
-        $router->get('/{id}/hirers', 'UserController@getHirers');
-        $router->post('/create', 'UserController@newUser');
-        $router->post('{id}/associateSystem', 'UserController@associateSystem');
-        $router->put('/{id}/update', 'UserController@updateUser');
-        $router->delete('/{id}/delete', 'UserController@deleteUser');
+    $router->group(['middleware' => 'regKey'], function () use ($router){
+        $router->post('/register', 'UserController@newUser');
     });
 
-    $router->group(['prefix' => 'hirer'], function ()  use ($router) {
-        $router->get('/getAll', 'HirerController@getAllHirers');
-        $router->get('/{id}/get', 'HirerController@getHirer');
-        $router->get('/{id}/systems', 'HirerController@getSystems');
-        $router->get('/{id}/users', 'HirerController@getUsers');
-        $router->get('/{id}/self', 'HirerController@getSelf');
-        $router->post('/create', 'HirerController@newHirer');
-        $router->post('/{id}/associateSystem', 'HirerController@associateSystem');
-        $router->post('/{id}/associateUser', 'HirerController@associateUser');
-        $router->put('/{id}/update', 'HirerController@updateHirer');
-        $router->delete('/{id}/delete', 'HirerController@deleteHirer');
-    });
+    $router->group(['middleware' => 'auth'], function () use ($router){
 
-    $router->group(['prefix' => 'system'], function ()  use ($router) {
-        $router->get('/getAll', 'SystemController@getAllSystems');
-        $router->get('/{id}/get', 'SystemController@getSystem');
-        $router->get('/{id}/hirers', 'SystemController@getHirers');
-        $router->get('/{id}/users', 'SystemController@getUsers');;
-        $router->post('/create', 'SystemController@newSystem');
-        $router->post('/{id}/associateHirer', 'SystemController@associateHirer');
-        $router->post('/{id}/associateUser', 'SystemController@associateUser');
-        $router->put('/{id}/update', 'SystemController@updateSystem');
-        $router->delete('/{id}/delete', 'SystemController@deleteSystem');
+        $router->post('refreshToken', 'AuthController@refreshToken');
+        $router->post('checkToken', 'AuthController@checkToken');
+        $router->post('logout', 'AuthController@logout');
+
+        $router->group(['prefix' => 'user'], function () use ($router) {
+            $router->group(['middleware' => 'sudoOnly'], function ()  use ($router) {
+                $router->get('/getAll', 'UserController@getAllUsers');
+            });
+
+            $router->group(['middleware' => 'sudoOrHirer'], function () use ($router){
+                $router->post('/create', 'UserController@newUser');
+                $router->post('{id}/associateSystem', 'UserController@associateSystem');
+                $router->delete('/{id}/delete', 'UserController@deleteUser');
+            });
+            $router->get('/{id}/get', 'UserController@getUser');
+            $router->get('/{id}/systems', 'UserController@getSystems');
+
+            $router->get('/{id}/hirers', 'UserController@getHirers');
+            $router->put('/{id}/update', 'UserController@updateUser');
+        });
+
+        $router->group(['prefix' => 'system', 'middleware' => 'sudoOnly'], function ()  use ($router) {
+            $router->get('/getAll', 'SystemController@getAllSystems');
+            $router->get('/{id}/get', 'SystemController@getSystem');
+            $router->get('/{id}/hirers', 'SystemController@getHirers');
+            $router->get('/{id}/users', 'SystemController@getUsers');;
+            $router->post('/create', 'SystemController@newSystem');
+            $router->post('/{id}/associateHirer', 'SystemController@associateHirer');
+            $router->post('/{id}/associateUser', 'SystemController@associateUser');
+            $router->put('/{id}/update', 'SystemController@updateSystem');
+            $router->delete('/{id}/delete', 'SystemController@deleteSystem');
+        });
+
+        $router->group(['prefix' => 'hirer'], function ()  use ($router) {
+            $router->group(['middleware' => 'sudoOnly'], function ()  use ($router) {
+                $router->get('/getAll', 'HirerController@getAllHirers');
+                $router->post('/create', 'HirerController@newHirer');
+                $router->post('/{id}/associateSystem', 'HirerController@associateSystem');
+                $router->put('/{id}/update', 'HirerController@updateHirer');
+                $router->delete('/{id}/delete', 'HirerController@deleteHirer');
+            });
+
+            $router->group(['middleware' => 'sudoOrHirer'], function ()  use ($router) {
+                $router->get('/{id}/get', 'HirerController@getHirer');
+                $router->get('/{id}/systems', 'HirerController@getSystems');
+                $router->get('/{id}/users', 'HirerController@getUsers');
+                $router->get('/{id}/self', 'HirerController@getSelf');
+
+                $router->post('/{id}/associateUser', 'HirerController@associateUser');
+
+                //falta implementar
+                $router->post('/{id}/createRegKey', 'HirerController@createRegKey');
+                $router->get('/{id}/getRegKey', 'HirerController@getRegKey');
+                $router->delete('/{id}/deleteRegKey', 'HirerController@deleteRegKey');
+            });
+        });
     });
 
 });
