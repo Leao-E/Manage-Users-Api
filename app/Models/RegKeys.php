14ebc96071f2eebbb2f4718b840c9dbceb4a039a
emanuelleao <eemanuelleao@gmail.com>
2020-08-13 11:20:39 -0300

    created a model for register key

--- app/Models/RegKeys.php
+++ app/Models/RegKeys.php
@@ -4,10 +4,25 @@
 namespace App\Models\AuxTables;
 
 
-class RegKeys
-{
-    public function getValidKeys()
-    {
+use App\Traits\Assets\DateUtil;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
+class RegKeys extends Model
+{
+    protected $primaryKey = "id";
+
+    protected $table = 'hre_reg_key';
+
+    protected $fillable = ['reg_key', 'name', 'hirer_id', 'dt_expire'];
+
+    protected $casts = [ 'dt_expire' => 'datetime' ];
+
+    public $timestamps = false;
+
+    static public function removeExpired(){
+        $now = DateUtil::now();
+        return DB::table('hre_reg_key')->where('dt_expire', '<', $now)->delete();
     }
+
 }
